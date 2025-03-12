77f1d3538bed11aa99b462cafc09f382e487cb0f
SAURABH RAJ <saurabhraj@MacBook-Pro.local>
2025-03-12 23:04:46 +0000

    created the endpoint for ocr

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # ai-ocr-api
 
 ```bash
-conda install -n ocr python=3.12
+conda create -n ocr python=3.12
 conda activate ocr
 ```
 
@@ -11,18 +11,20 @@ cd aimw
 
 Install tesseract-ocr engine:
 For Ubuntu:
+
 ```bash
 sudo apt-get install tesseract-ocr
 ```
+
 For windows:
 https://github.com/UB-Mannheim/tesseract/wiki
 
 For Mac:
+
 ```bash
-$ brew install tesseract
+brew install tesseract
 ```
 
 ```bash
 pip install -r requirements.txt
 ```
-
--- aimw/app/apis/endpoints/ocr.py
+++ aimw/app/apis/endpoints/ocr.py
@@ -1,9 +1,49 @@
-from fastapi import APIRouter
-from PIL import Image
+import shutil
+import uuid
+from pathlib import Path
+
 import pytesseract
+from app.database import SessionLocal
+from app.models.ocr_results import OcrResults
+from app.services.read_image import read_image
+from fastapi import APIRouter, BackgroundTasks, Depends, File, UploadFile
+from PIL import Image
+from sqlalchemy.orm import Session
 
 router = APIRouter()
 
-router.post("/ocr")
-def perform_ocr(request):
-    return pytesseract.image_to_string(Image.open(request))
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    except:
+        db.close()
+
+
+@router.post("/ocr", response_model=dict)
+async def perform_ocr(file: UploadFile = File(...), db: Session = Depends(get_db)):
+    file_extension = file.filename.split(".")[-1]
+    unique_filename = f"{uuid.uuid4()}.{file_extension}"
+    file_location = Path("temp_files") / unique_filename
+
+    with open(file_location, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+
+    text = read_image(file_location)
+
+    # saving ocr result in db:
+    ocr_record = OcrResults()
+    ocr_record.file_name = unique_filename
+    ocr_record.text = text
+    db.add(ocr_record)
+    db.commit()
+    db.refresh(ocr_record)
+
+    # removing temp:
+    file_location.unlink()
+    return {
+        "id": ocr_record.id,
+        "filename": ocr_record.filename,
+        "text": ocr_record.text,
+    }
